abfcf5d2fe2a797cc0dac6237c08f31f5f3f9ed4
veejask-41 <sajeevk2001@gmail.com>
2024-06-08 00:51:34 +0530

    created text, textfield, button widgets for currency converter

--- currency_converter/lib/currency_converter_material_page.dart
+++ currency_converter/lib/currency_converter_material_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget {
@@ -5,8 +6,75 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.black,
+        width: 2.0,
+        style: BorderStyle.solid, // default
+        strokeAlign: BorderSide.strokeAlignInside, // default
+      ),
+      borderRadius: BorderRadius.circular(10),
+    );
+
     return Scaffold(
-      body: Center(child: Text('Hello, World!!!!!')),
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                '0',
+                style: TextStyle(
+                    fontSize: 55,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 255, 255, 255)),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: TextField(
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Enter the amount in USD',
+                    hintStyle: const TextStyle(
+                      color: Colors.black,
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.monetization_on,
+                    ),
+                    prefixIconColor: Colors.black,
+                    filled: true,
+                    fillColor: Colors.white,
+                    focusedBorder: border,
+                    enabledBorder: border,
+                  ),
+                  keyboardType: const TextInputType.numberWithOptions(
+                    decimal: true,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton(
+                  onPressed: () {
+                    if (kDebugMode) {
+                      print('btn clicked');
+                    }
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                  child: const Text('Convert!'),
+                ),
+              )
+            ]),
+      ),
     );
   }
 }
