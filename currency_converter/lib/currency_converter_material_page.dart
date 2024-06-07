8790a3b6e043177b18afa8be6f716f4275c34813
veejask-41 <sajeevk2001@gmail.com>
2024-06-08 01:00:07 +0530

    created appbar for currency converter

--- currency_converter/lib/currency_converter_material_page.dart
+++ currency_converter/lib/currency_converter_material_page.dart
@@ -18,6 +18,12 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        elevation: 0,
+        title: const Text('Currency Converter'),
+        centerTitle: true,
+      ),
       body: Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
