d1fe86e37023beafcae6cdb72494b8e40a8ca12a
veejask-41 <sajeevk2001@gmail.com>
2024-06-08 20:53:39 +0530

    finished currency converter material app

--- currency_converter/lib/currency_converter_material_page.dart
+++ currency_converter/lib/currency_converter_material_page.dart
@@ -1,8 +1,17 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget {
+class CurrencyConverterMaterialPage extends StatefulWidget {
   const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  State<CurrencyConverterMaterialPage> createState() =>
+      _CurrencyConverterMaterialPageState();
+}
+
+class _CurrencyConverterMaterialPageState
+    extends State<CurrencyConverterMaterialPage> {
+  double result = 0;
+  TextEditingController textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,9 +38,9 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const Text(
-                '0',
-                style: TextStyle(
+              Text(
+                'LKR ${result!=0 ? result.toStringAsFixed(2): '0'}',
+                style: const TextStyle(
                     fontSize: 55,
                     fontWeight: FontWeight.bold,
                     color: Color.fromARGB(255, 255, 255, 255)),
@@ -39,6 +48,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.all(8),
                 child: TextField(
+                  controller: textEditingController,
                   style: const TextStyle(
                     color: Colors.black,
                   ),
@@ -65,9 +75,10 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                 padding: const EdgeInsets.all(8.0),
                 child: TextButton(
                   onPressed: () {
-                    if (kDebugMode) {
-                      print('btn clicked');
-                    }
+                    setState(() {
+                      result =
+                          double.parse(textEditingController.text) * 304.70;
+                    });
                   },
                   style: TextButton.styleFrom(
                     backgroundColor: Colors.black,
